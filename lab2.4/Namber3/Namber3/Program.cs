4eb111c96e2f7e0526c1beb3d05d857e20edfc28
maksimka212 <99385672+maksimka212@users.noreply.github.com>
2022-02-14 11:20:53 +0300

    Add files via upload

---
+++ lab2.4/Namber3/Namber3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+namespace Namber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine();
+            int a = Convert.ToInt32(ReadLine());
+            int b = Convert.ToInt32(ReadLine());
+            int A = a;
+            int B = b;
+            int n_a = 0;
+            int n_b = 0;
+            do
+            {
+                a = a / 10;
+                n_a++;
+            } while (a > 0);
+            
+            do
+            {
+                b = b / 10;
+                n_b++;
+            } while (b > 0);
+            if (n_a > n_b)
+            {
+                WriteLine(A);
+            }
+            if (n_a < n_b)
+            {
+                WriteLine(B);
+            }
+            else
+                WriteLine("Они равны.");
+        }
+    }
+}
